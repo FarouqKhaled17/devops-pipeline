e64aecfb39eea769322f3e59a5bd5ea7b984ce2f
FarouqKhaled17 <faroukkhaled45@gmail.com>
2026-01-13 23:39:58 +0200

    feat: add basic arithmetic tests to test.js

--- test.js
+++ test.js
@@ -4,3 +4,11 @@ console.log("Running tests...");
 setTimeout(() => {
     console.log("Tests completed.");
 }, 3000);
+
+test('basic arithmetic passes', () => {
+    expect(1 + 1).toBe(2);
+});
+
+test('basic arithmetic fails', () => {
+    expect(1 + 1).toBe(3);
+});
