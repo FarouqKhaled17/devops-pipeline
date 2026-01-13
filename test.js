c51d402c307d066c90fd90092b3aaca43f1c5a96
FarouqKhaled17 <faroukkhaled45@gmail.com>
2026-01-13 23:42:38 +0200

    fix: remove failing arithmetic test and adjust timeout handling

--- test.js
+++ test.js
@@ -3,12 +3,8 @@ console.log("This is the test.js file.");
 console.log("Running tests...");
 setTimeout(() => {
     console.log("Tests completed.");
-}, 3000);
+}, 3000).unref();
 
 test('basic arithmetic passes', () => {
     expect(1 + 1).toBe(2);
-});
-
-test('basic arithmetic fails', () => {
-    expect(1 + 1).toBe(3);
 });
