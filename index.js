8d1b9e26beb400db11597e5a13cfa401e5118545
FarouqKhaled17 <faroukkhaled45@gmail.com>
2026-01-14 18:03:14 +0200

    fix: remove unnecessary comment from index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ console.log('This is the index.js file.');
 console.log('Welcome to the project!');
 console.log('Enjoy coding!');
 
-// minimal app used by "npm start"
 console.log('App started');
 setTimeout(() => {
     console.log('Exiting (short-lived app for CI)');
